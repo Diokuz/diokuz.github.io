306118a038f32dfe59302da83a732eff6d97ec09
Kuznetsov Dmitry <dm.kuznecov@2gis.ru>
2013-03-01 16:15:47 +0700

    Demo update

--- baron/script.js
+++ baron/script.js
@@ -1,13 +1,14 @@
 window.onload = function() {
-    baron($('.wrapper'), {
+    baron($('.wrapper_1'), {
         scroller: '.scroller',
         container: '.container',
         bar: '.scroller__bar',
-        barTop: 20,
         barOnCls: 'scroller__bar_state_on',
         header: '.header__title',
         hFixCls: 'header__title_state_fixed',
         hTopFixCls: 'header__title_position_top',
         hBottomFixCls: 'header__title_position_bottom'
     });
+
+    $('.wrapper_2').baron({barOnCls: 'scroller__bar_state_on'});
 };
